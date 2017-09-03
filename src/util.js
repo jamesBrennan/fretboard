4bab87c0ddb51179ddce8048318f00548ca26d1b
James Brennan <james.brennan@educationsuperhighway.org>
2017-09-03 13:39:06 -0700

    Add merge function

--- src/util.js
+++ src/util.js
@@ -12,3 +12,25 @@ export function range(a, b) {
 export function positionRatio(position) {
   return range(position).reduce((prev => prev / 1.05946), 1);
 }
+
+export function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item) && item !== null);
+}
+
+export function uniq(array) { return [ ...new Set(array) ]; }
+
+export function merge(a, b) {
+  if (isObject(a) && isObject(b)) {
+    let keys = uniq(Object.keys(a).concat(Object.keys(b)));
+    return keys.reduce((obj, key) => {
+      if(b.hasOwnProperty(key)) {
+        obj[key] = merge(a[key], b[key]);
+      }
+      else {
+        obj[key] = a[key];
+      }
+      return obj;
+    }, {});
+  }
+  return b;
+}
